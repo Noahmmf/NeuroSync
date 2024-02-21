1b3d993f8b2927fcde7002d7bab1912140269bf5
Noahmmf <noahfuglestad@gmail.com>
2024-02-21 12:23:30 -0600

    able to add to household in postman, need to figure out how to add constriants to allow unique household keys for partner to be added to household.

--- server/routes/household.router.js
+++ server/routes/household.router.js
@@ -1,75 +1,104 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 const {
   rejectUnauthenticated,
-} = require('../modules/authentication-middleware');
+} = require("../modules/authentication-middleware");
 
 /**
 Get request for goals section. rejectUnauthenticated user will reject anyone
 who is not the logged in user. 
  */
-router.get('/', rejectUnauthenticated, (req, res) => {
-  const queryText = '';
+router.get("/", rejectUnauthenticated, (req, res) => {
+  const queryText = "";
 
-  pool.query(queryText, [req.user.id])
-  .then((result) => res.send(result.rows))
-  .catch((err) => {
-    console.log('Error in GET server:', err);
-    res.sendStatus(500);
-  })
+  pool
+    .query(queryText, [req.user.id])
+    .then((result) => res.send(result.rows))
+    .catch((err) => {
+      console.log("Error in GET server:", err);
+      res.sendStatus(500);
+    });
 });
 
 /**
  * POST route template
  */
-router.post('/', rejectUnauthenticated, (req, res) => {
-  const queryText = ``;
+router.post("/", rejectUnauthenticated, (req, res) => {
+  const userId = req.user.id;
 
-  console.log(`here is the insert for Calendar:`);
+  const insertHouseholdName = `
+    INSERT INTO "household"
+    ("name", "household_key")
+    VALUES
+    ($1, $2)
+    RETURNING "id";
+    `;
+  const householdName = [req.body.name, req.body.household_key];
 
-  pool.query(queryText, [])
-  .then(() => res.sendStatus(201))
-  .catch((err) => {
-    console.log("Can't add to Calendar", err);
-    res.sendStatus(500);
+  pool.query(insertHouseholdName, householdName).then((result) => {
+    const createdHouseholdId = result.rows[0].id;
+
+    console.log("new Household is:", createdHouseholdId);
+
+    const newHouseholdMembers = `
+    INSERT INTO  "household_members"
+    ("household_id", "user_id")
+    VALUES
+    ($1, $2);
+    `;
+
+    pool
+      .query(newHouseholdMembers, [createdHouseholdId, userId])
+      .then((result) => {
+        //Displays whats being sent back
+
+        res.sendStatus(201);
+      })
+      .catch((err) => {
+        // catch for second query
+        console.log(err);
+        res.sendStatus(500);
+      });
   });
 });
 
 /**
  * Delete an item if it's something the logged in user added the goal
  */
-router.delete('/:id', rejectUnauthenticated, (req, res) => {
+router.delete("/:id", rejectUnauthenticated, (req, res) => {
   const taskId = req.params.id;
-//   const user = req.user.id;
-
-  const query = `
-
-  `
-
-  pool
-  .query(query, [])
-  .then(() => res.sendStatus(204))
-  .catch((err) => { console.log("Error deleteing task", err);
-  res.sendStatus(500);
-})
-});
-
-router.put('/:id', rejectUnauthenticated, (req, res) => {
-//   const user = req.user.id;
-  const task = req.body
+  //   const user = req.user.id;
 
   const query = `
 
   `;
 
-    console.log(`This is what I am updating:`, req.body);
+  pool
+    .query(query, [])
+    .then(() => res.sendStatus(204))
+    .catch((err) => {
+      console.log("Error deleteing task", err);
+      res.sendStatus(500);
+    });
+});
+
+router.put("/:id", rejectUnauthenticated, (req, res) => {
+  //   const user = req.user.id;
+  const task = req.body;
+
+  const query = `
+
+  `;
+
+  console.log(`This is what I am updating:`, req.body);
 
   pool
-  .query(query, [])
-  .then(() => res.sendStatus(204))
-  .catch((err) => { console.log("Error updating task", err);
-  res.sendStatus(500);
-})
+    .query(query, [])
+    .then(() => res.sendStatus(204))
+    .catch((err) => {
+      console.log("Error updating task", err);
+      res.sendStatus(500);
+    });
 });
 module.exports = router;
